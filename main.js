fa13959a517a27e6e8ed33adf71aef29e4aee980
Luan Saboia <luan.saboia@gmail.com>
2022-05-26 14:34:06 -0300

    finalizando mobileFirst

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if(scrollY > 0){
     navigation.classList.add('scroll')
   } else navigation.classList.remove('scroll')
+}
+
+function showBackToTopButtonOnScroll() {
+  if(scrollY > 400 && scrollY < 4555){
+    backToTopButton.classList.add('show')
+  } else backToTopButton.classList.remove('show')
 }
 
 function openMenu() {
@@ -11,3 +25,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
